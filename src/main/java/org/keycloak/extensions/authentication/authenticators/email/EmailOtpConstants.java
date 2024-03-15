a6be98cd89432783ce35ee2ea08c495adaa57636
skydrinker_tox <skydrinker_tox@hotmail.com>
2024-03-15 08:21:46 +0100

    🔨 Add ability to configure a hardcoded OTP and to disable mailing (for development purpose)

--- src/main/java/org/keycloak/extensions/authentication/authenticators/email/EmailOtpConstants.java
+++ src/main/java/org/keycloak/extensions/authentication/authenticators/email/EmailOtpConstants.java
@@ -6,9 +6,13 @@ public class EmailOtpConstants {
 	public static final String CODE_LENGTH = "length";
 	public static final String CODE_TTL = "ttl";
 	public static final String RESEND_WAIT_DELAY = "resendWaitDelay";
+	public static final String HARDCODED_OTP = "hardcodedOtp";
+	public static final String DISABLE_MAILING = "disableMailing";
 	public static final String PREVIOUS_SENT_TIME = "codeSentAt";
 	public static final int DEFAULT_LENGTH = 6;
 	public static final int DEFAULT_TTL = 300;
 	public static final int DEFAULT_RESEND_WAIT_DELAY = 0;
+	public static final boolean DEFAULT_DISABLE_MAILING = false;
+
 
 }
